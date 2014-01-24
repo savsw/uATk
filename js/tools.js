cffdad922feeffbfdc846baf022f71e2b59d7e6f
Matthew R Chase <matt@chasefox.net>
2014-01-24 14:01:23 -0500

    tweaking adjusted license notice.

--- js/tools.js
+++ js/tools.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
 * unofficial ANTLabs toolkit (uATk) - a chrome extension.
-* Copyright 2013 Savannah Scriptworks (savsw.org).
+* Copyright 2014 Savannah Scriptworks (savsw.org).
 *
 *    This program is free software: you can redistribute it and/or modify
 *    it under the terms of the GNU General Public License as published by
